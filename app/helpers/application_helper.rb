66baaabc2b8272fcaa93b807c75b1be6d5e4b5f0
Xuxa <rodrigo@preface.com.br>
2018-10-26 12:06:08 -0500

    Add css at body to identify each page easily

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
 		return 'active' if request.path == test_path
 		''
 	end
+	
+	# Outputs the body classes and adds one that
+	# is automatically generated
+	# ex: 'admin-users-index-view'
+	def body_css_class
+	  @body_css_classes ||= []
+	  view_css_class = [controller_path.split('/'), action_name, 'view'].flatten.join('-')
+
+	  @body_css_classes.unshift(view_css_class).join(' ')
+	end
 end
