a907b5c1c3262dbb6ee293887e658b728af3823e
Xuxa <rodrigo@preface.com.br>
2020-04-27 16:09:59 -0300

    Add mail address

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -3,7 +3,7 @@
 class SystemMailer < ActionMailer::Base
 	#add_template_helper(StoreHelper)
 	#add_template_helper(ApplicationHelper)
-						
+
 	default		:from => "Yamaguishi Orgânicos <produtos@yamaguishi.com.br>",
 						:reply_to => "Yamaguishi Orgânicos <produtos@yamaguishi.com.br>",
 						:return_path => "Yamaguishi Orgânicos <produtos@yamaguishi.com.br>"
@@ -12,13 +12,13 @@ class SystemMailer < ActionMailer::Base
 		@order = order
 		@client = client
 		mail(:to => "#{@client.name} <#{@client.email}>",
-			:bcc => "siteyamaguishi@gmail.com, produtos@yamaguishi.com.br",
+			:bcc => "siteyamaguishi@gmail.com, produtos@yamaguishi.com.br, vila.yamaguishi@gmail.com",
 			:subject => "Confirmação de Pedido ##{@order.id.to_s.rjust(6, '0')}",
 			:reply_to => "#{@client.name} <#{@client.email}>")
 	end
-	
+
 	def client_changed(old_name, old_kind, old_document, old_email, old_phone, old_add, new)
-		
+
 		@old_name = old_name
 		@old_kind = old_kind
 		@old_document = old_document
@@ -28,17 +28,17 @@ class SystemMailer < ActionMailer::Base
 
 		@new = new
 		mail(:to => "produtos@yamaguishi.com.br",
-			:bcc => "siteyamaguishi@gmail.com",
+			:bcc => "siteyamaguishi@gmail.com, vila.yamaguishi@gmail.com",
 			:subject => "Alteração de Cliente ##{@new.yamasis_id.to_s.rjust(6, '0')} [#{@new.name}]",
 			:reply_to => "#{@new.name} <#{@new.email}>")
 	end
-	
+
 	def contact_form(contact)
 		@contact = contact
 		mail	:subject => "[Mensagem do Site] #{contact.subject}",
 					:to => "produtos@yamaguishi.com.br",
-					:bcc => "siteyamaguishi@gmail.com",
+					:bcc => "siteyamaguishi@gmail.com, vila.yamaguishi@gmail.com",
 					:reply_to => "#{@contact.name} <#{@contact.email}>"
 	end
-	
+
 end
