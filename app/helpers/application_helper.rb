ab0d423ff6079591b0d9bbca3257d4da4aa40ee0
Xuxa <rodrigo@preface.com.br>
2020-05-27 18:25:25 -0300

    Add one more page and fix properly old links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 		return 'active' if request.path == test_path
 		''
 	end
-	
+
 	# Outputs the body classes and adds one that
 	# is automatically generated
 	# ex: 'admin-users-index-view'
@@ -12,5 +12,9 @@ module ApplicationHelper
 	  view_css_class = [controller_path.split('/'), action_name, 'view'].flatten.join('-')
 
 	  @body_css_classes.unshift(view_css_class).join(' ')
+	end
+
+	def action?(*action)
+		action.include?(params[:action])
 	end
 end
