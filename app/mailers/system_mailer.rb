c6042a17fa1571ccf9331750dc7cd83fc49040cc
Xuxa <rodrigo@preface.com.br>
2020-06-18 10:57:49 -0300

    Fix mails

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -12,7 +12,7 @@ class SystemMailer < ActionMailer::Base
 		@order = order
 		@client = client
 		mail(:to => "#{@client.name} <#{@client.email}>",
-			:bcc => "produtos@yamaguishi.com.br",
+			:bcc => "siteyamaguishi@gmail.com, produtos@yamaguishi.com.br",
 			:subject => "Confirmação de Pedido ##{@order.id.to_s.rjust(6, '0')}",
 			:reply_to => "#{@client.name} <#{@client.email}>")
 	end
@@ -21,6 +21,7 @@ class SystemMailer < ActionMailer::Base
 		@order = order
 		@client = client
 		mail(:to => "produtos@yamaguishi.com.br",
+			:bcc => "siteyamaguishi@gmail.com",
 			:subject => "Cancelamento do Pedido ##{@order.id.to_s.rjust(6, '0')}",
 			:reply_to => "#{@client.name} <#{@client.email}>")
 	end
@@ -36,6 +37,7 @@ class SystemMailer < ActionMailer::Base
 
 		@new = new
 		mail(:to => "produtos@yamaguishi.com.br",
+			:bcc => "siteyamaguishi@gmail.com",
 			:subject => "Alteração de Cliente ##{@new.yamasis_id.to_s.rjust(6, '0')} [#{@new.name}]",
 			:reply_to => "#{@new.name} <#{@new.email}>")
 	end
@@ -44,6 +46,7 @@ class SystemMailer < ActionMailer::Base
 		@contact = contact
 		mail	:subject => "[Mensagem do Site] #{contact.subject}",
 					:to => "produtos@yamaguishi.com.br",
+					:bcc => "siteyamaguishi@gmail.com",
 					:reply_to => "#{@contact.name} <#{@contact.email}>"
 	end
 
