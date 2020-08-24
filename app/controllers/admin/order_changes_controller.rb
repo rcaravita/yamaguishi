d0c970859e6abe5ec4cffea5481ca81ba19b1208
Xuxa <rodrigo@preface.com.br>
2020-08-24 11:41:08 -0300

    Limit results

--- app/controllers/admin/order_changes_controller.rb
+++ app/controllers/admin/order_changes_controller.rb
@@ -3,7 +3,7 @@ class Admin::OrderChangesController < AdminController
 	# GET /admin/changes
 	# GET /admin/changes.json
 	def index
-		@admin_changes = Admin::OrderChange.order("id DESC")
+		@admin_changes = Admin::OrderChange.order("id DESC").limit(100)
 
 		respond_to do |format|
 			format.html # index.html.erb
